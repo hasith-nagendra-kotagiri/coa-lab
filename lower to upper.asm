f89d3a73063d1d3e8535b429b5183436aa644c25
hasith-nagendra-kotagiri <160167791+hasith-nagendra-kotagiri@users.noreply.github.com>
2024-09-26 21:26:53 +0530

    Update lower to upper.asm

--- lower to upper.asm
+++ lower to upper.asm
@@ -1,17 +1,53 @@
-org 100h        ; Set program origin for COM file
-; Load the lower-case letter into AL
-mov al,'a'     ; Lower-case letter to convert
-; Convert to upper-case by subtracting 32 (20h)
-sub al,20h     ; Subtract 32 to get the corresponding upper-case letter
-; Store the result in DL to print
-mov dl,al      ; Move the upper-case character into DL
-; Set AH to 02h (function for displaying character)
-mov ah,02h     ; DOS interrupt function to print a character
-; Call the interrupt to print the character
-int 21h         ; Interrupt to print the character
+ORG 100h           ; Origin, to specify that the program starts at 100h (COM file format)
+
+; Display message "Enter an uppercase letter: "
+MOV DX, OFFSET msg_input  ; Load the address of the message
+MOV AH, 09h        ; Function 09h of INT 21h is used to display a string
+INT 21h            ; Call DOS interrupt to print the message
+
+; Read a single character from the user
+MOV AH, 01h        ; Function 01h of INT 21h is used to read a character
+INT 21h            ; Call DOS interrupt to get the character
+MOV DL, AL         ; Store the input character in AL
+
+; Check if the character is an uppercase letter (A-Z)
+CMP AL, 'a'        ; Compare AL with 'A'
+JL NotUpperCase    ; If the input is less than 'A', it is not uppercase
+CMP AL, 'z'        ; Compare AL with 'Z'
+JG NotUpperCase    ; If the input is greater than 'Z', it is not uppercase
+
+; Convert the uppercase letter to lowercase
+sub AL, 20h        ; sub 32 (20h) to convert uppercase to lowercase
+MOV BL,AL
+
+; Print the message "The lowercase letter is: "
+MOV DX, OFFSET msg_output  ; Load the address of the output message
+MOV AH, 09h        ; Function 09h of INT 21h is used to display a string
+INT 21h            ; Call DOS interrupt to print the output message
+MOV AL,BL
+
+ ; Print the converted lowercase letter
+MOV DL, AL         ; Move the lowercase letter to DL
+MOV AH, 02h        ; Function 02h of INT 21h is used to print a single character
+INT 21h            ; Call DOS interrupt to print the character
+JMP EndProgram     ; Jump to the end of the program
+
+NotUpperCase:
+; If the input is not an uppercase letter, display an error message
+MOV DX, OFFSET msg_error  ; Load the address of the error message
+MOV AH, 09h        ; Function 09h of INT 21h is used to display a string
+INT 21h            ; Call DOS interrupt to print the error message
+
+EndProgram:
 ; Terminate the program
-mov ah,4Ch     ; DOS interrupt function to exit program
-int 21h         ; Terminate the program
+MOV AH, 4Ch        ; Function 4Ch of INT 21h terminates the program
+INT 21h            ; Call DOS interrupt to exit
+
+msg_input  DB 'Enter an lower case letter: $'
+msg_output DB 0Dh, 0Ah, 'The uppercase letter is: $'  ; Output message
+msg_error  DB 0Dh, 0Ah, 'Error: Not an lower case letter! $'  ; Error message
+
+END                ; End of program
 
 
 
